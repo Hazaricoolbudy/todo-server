63dc35c31f2830eea41d3a48ba00bb77d14b7065
Ankush <ank123.rana@gmail.com>
2023-03-29 12:49:42 +0530

    backend is completed

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const connectToMongo = require('./db')
+const cors = require('cors')
 connectToMongo()
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 8000;
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
